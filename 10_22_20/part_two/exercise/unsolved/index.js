cae72da6b0d6ec49d6d91b154b744d7b09ae61d4
Margaret Kellogg <53274071+multifaceted1@users.noreply.github.com>
2020-10-27 21:12:12 -0400

    Add files via upload

---
+++ 10_22_20/part_two/exercise/unsolved/index.js
@@ -0,0 +1,13 @@
+/*
+  When the left arrow is clicked, go to the previous slide:
+  1. access the selector for the left arrow using jquery
+  2. attach an on click function to that element
+  3. in between the brackets, using bootstrap js to access the carousel, add the 'prev' attribute to the carousel
+
+  When the right arrow is clicked, go to the next slide:
+  1. access the selector for the right arrow using jquery
+  2. attach an on click function to that element
+  3. in between the brackets, using bootstrap js to access the carousel, add the 'next' attribute to the carousel
+
+  Look in the bootstrap docs for the js functions you can use with a carousel
+*/
